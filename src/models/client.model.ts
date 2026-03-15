8fbc6f30c11d0fef0d60376cca977f5707448969
Abigaelchemtai <118007085+Abigaelchemtai@users.noreply.github.com>
2026-03-15 14:16:12 +0300

    Remove commented lines in client model

--- src/models/client.model.ts
+++ src/models/client.model.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
-//The Client Schema
+
 const clientSchema = new mongoose.Schema({
-  clientId: { type: String, required: true, unique: true }, // unique identifier
+  clientId: { type: String, required: true, unique: true }, 
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   dateOfBirth: { type: Date, required: true },
   address: { type: String, required: true },
 });
 
-// Export the model
+
 export default mongoose.model("Client", clientSchema);
